032103da810c69228c757ee85531408840b21e3e
Tan Jun Yan <yan@yan3321.com>
2026-03-03 16:57:10 +0800

    build(deps): upgrade prisma to v7

--- src/postgres.ts
+++ src/postgres.ts
@@ -1,7 +1,12 @@
 import { payout_requests, PrismaClient } from "@prisma/client";
 import config from "./config.js";
+import pg from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const prisma = new PrismaClient();
+const { Pool } = pg;
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 // TODO: replace this unsafe method of serialisation
 BigInt.prototype.toJSON = function () {
